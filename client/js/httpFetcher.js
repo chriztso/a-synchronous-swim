286736879871b0fe8355964c5eef3e614a30d21f
chriztso <christophertso2017@u.northwestern.edu>
2019-01-22 15:01:02 -0800

    Implemented bare minimum requirements

--- client/js/httpFetcher.js
+++ client/js/httpFetcher.js
@@ -1,5 +1,17 @@
 (function() {
+  const serverUrl = 'http://127.0.0.1:3000';
+  $.ajax({
+    url: serverUrl, 
+    type: 'GET',
 
-  const serverUrl = '?';
+    success: function(data){
+      data = JSON.parse(data);
+      SwimTeam.move(data);
+    },
+    error: function(){
+      console.log('Failed');
+    }
+    
+  })
 
-})();
+})
--- client/js/keypressHandler.js
+++ client/js/keypressHandler.js
@@ -7,4 +7,30 @@ $('body').on('keydown', (event) => {
   }
 });
 
+$('.RandomClick').on('click', (event) => {
+
+  const serverUrl = 'http://127.0.0.1:3000';
+  $.ajax({
+    url: serverUrl, 
+    type: 'GET',
+    contentType: 'application/json',
+    success: function(data){
+        console.log("Correct");
+      data = JSON.parse(data);
+      SwimTeam.move(data);
+    },
+    error: function(){
+      console.log('Failed');
+    }
+    
+  })
+
+
+  // var directions = ['Up', 'Down', 'Left', 'Right'];
+  // var randomIndex =  Math.floor(Math.random() * directions.length);
+  // var directionToGo = directions[randomIndex];
+  //    SwimTeam.move(directionToGo.toLowerCase());
+  
+  });
+
 console.log('Client is running in the browser!');
